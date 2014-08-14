a35715e3bb72d789730f94b7eb5a805bde2d0281
= <=>
2014-08-13 20:55:57 -0400

    added simple cherrypy api call

--- lib/scraper.py
+++ lib/scraper.py
@@ -4,4 +4,4 @@ from lib.requesters.ignrequester import IgnRequester
 def scrape(game_name):
     x = IgnRequester(game_name)
     x.scrape_item()
-    print x.get_item_as_json()
+    return x.get_item_as_json()
--- run.py
+++ run.py
@@ -1,5 +1,19 @@
 from lib import scraper
+import cherrypy
 
-print scraper.scrape('banjo kazooie')
+
+class Root(object):
+    @cherrypy.expose
+    def index(self):
+        return "Usage:"
+
+    @cherrypy.expose
+    def reviews(self, game):
+        print game
+        ret = scraper.scrape(game)
+        return ret
+
+if __name__ == '__main__':
+   cherrypy.quickstart(Root(), '/')
 
 
