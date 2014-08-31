45e75f164aff519a142e9272de27f7e27eadffec
oakesja <jacoboakes92@gmail.com>
2014-08-31 12:02:01 -0400

    added more xpaths for game information

--- lib/paths/gamespot_paths.py
+++ lib/paths/gamespot_paths.py
@@ -1,3 +1,4 @@
+# Review Information
 def GamespotRatingXpath():
     return "//*[contains(concat(' ', normalize-space(@class), ' '), ' reviewObject__gs ')]//*[contains(concat(' ', normalize-space(@class), ' '), ' gs-score__cell ')]//*[contains(concat(' ', normalize-space(@itemprop), ' '), 'ratingValue')]/text()"
 
@@ -10,9 +11,44 @@ def GamespotCommunityRatingXpath():
     return "//*[contains(concat(' ', normalize-space(@class), ' '), ' breakdown-userScore ')]//*[contains(concat(' ', normalize-space(@class), ' '), ' breakdown-avgScore ')]//*[contains (concat(' ', normalize-space(@class), ' '), ' gs-score__cell ')]/text()"
 
 
+def GamespotReviewLinkXpath():
+    return "//*[contains(concat(' ', normalize-space(@class), ' '), ' media media-review ')][1]//a/@href"
+
+
+# Game Information
+def GamespotGameNameXpath():
+    return "//*[contains(concat(' ', normalize-space(@class), ' '), ' gameObject__title ')]/a/text()"
+
+
+def GamespotPlatformsXpath():
+    return "//*[contains(concat(' ', normalize-space(@class), ' '), ' gameObject__info ')]//*[contains(concat(' ', normalize-space(@class), ' '), ' system-list ')]/li/a/text()"
+
+
+def GamespotReleaseDateXpath():
+    return "//*[contains(concat(' ', normalize-space(@class), ' '), ' gameObject__info ')]//*[contains(concat(' ', normalize-space(@itemprop), ' '), ' datePublished ')]/text()"
+
+
+def GamespotGameDescriptionXpath():
+    return "//*[contains(concat(' ', normalize-space(@class), ' '), ' secondary-content ')]//*[contains(concat(' ', normalize-space(@class), ' '), ' pod-objectStats-info__deck ')]/text()"
+
+
+def GamespotGenre():
+    return "//*[contains(concat(' ', normalize-space(@class), ' '), ' secondary-content ')]//*[contains(concat(' ', normalize-space(@class), ' '), ' pod-objectStats-additional ')]/dd[3]/a/text()"
+
+
+def GamespotDeveloperXpath():
+    return "//*[contains(concat(' ', normalize-space(@class), ' '), ' secondary-content ')]//*[contains(concat(' ', normalize-space(@class), ' '), ' pod-objectStats-additional ')]/dd[1]/a/text()"
+
+
+def GamespotPublisherXpath():
+    return "//*[contains(concat(' ', normalize-space(@class), ' '), ' secondary-content ')]//*[contains(concat(' ', normalize-space(@class), ' '), ' pod-objectStats-additional ')]/dd[2]/a/text()"
+
+
 def GamespotPictureLinkXpath():
     return "//*[contains(concat(' ', normalize-space(@class), ' '), ' gameObject__img ')]//img/@src"
 
 
-def GamespotReviewLinkXpath():
-    return "//*[contains(concat(' ', normalize-space(@class), ' '), ' media media-review ')][1]//a/@href"
+def GamespotEsrbLinkXpath():
+    return "//*[contains(concat(' ', normalize-space(@class), ' '), ' secondary-content ')]//*[contains(concat(' ', normalize-space(@class), ' '), ' pod-objectStats__esrb ')]/img/@src"
+
+
--- lib/paths/ign_paths.py
+++ lib/paths/ign_paths.py
@@ -1,3 +1,4 @@
+# Review Information
 def IgnRatingXpath():
     return "//*[contains(@class, 'ignRating ratingRow')]//*[contains(@class, 'ratingValue')]/text()"
 
@@ -14,10 +15,6 @@ def IgnCommunitRatingDescriptionXpath():
     return "//*[contains(@class, 'communityRating ratingRow')]//*[contains(@class, 'ratingText')]/text()"
 
 
-def IgnPictureLinkXpath():
-    return "//*[contains(@class, 'mainBoxArt')]/img/@src"
-
-
 def IgnReviewLinkXpath():
     return "//*[contains(@class, 'ignRating ratingRow')]/a/@href"
 
@@ -26,5 +23,40 @@ def IgnVidoeReviewLinkXpath():
     return "//*[contains(@id, 'mainArticle')]//*[contains(@class, 'highlight-links clear')]/a/@href"
 
 
+# Game Information
+def IgnGameNameXpath():
+    return "//*[contains(concat(' ', normalize-space(@class), ' '), ' contentTitle ')]/a/text()"
+
+
+def IgnPlatformXpaths():
+    return "//*[contains(concat(' ', normalize-space(@class), ' '), ' contentPlatformsText ')]/span/a/text()"
+
+
+def IgnReleaseDateXpath():
+    return "//*[contains(concat(' ', normalize-space(@class), ' '), ' releaseDate ')]/strong/text()"
+
+
+def IgnGameDescriptionXpath():
+    return "//*[contains(concat(' ', normalize-space(@class), ' '), ' gameInfo ')]/p/text()"
+
+
+def IgnGenre():
+    return "//*[contains(concat(' ', normalize-space(@class), ' '), ' gameInfo-list ')][2]/div[1]/a/text()"
+
+
+def IgnDeveloperXpath():
+    return "//*[contains(concat(' ', normalize-space(@class), ' '), ' gameInfo-list ')][2]/div[2]/a/text()"
+
+
+def IgnPublisherXpath():
+    return "//*[contains(concat(' ', normalize-space(@class), ' '), ' gameInfo-list ')][2]/div[3]/a/text()"
+
+
+def IgnPictureLinkXpath():
+    return "//*[contains(concat(' ', normalize-space(@class), ' '), ' mainBoxArt ')]/img/@src"
+
+
 def IgnEsrbLinkXpath():
     return "//*[contains(@id, 'summary')]/div[@class='gameInfo']/p/a/@href"
+
+
